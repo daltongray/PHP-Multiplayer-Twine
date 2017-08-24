b5bcca0a8a876ce22950697be0058ca5a84e123d
daltongray <dalton.roger.gray@gmail.com>
2017-08-24 17:04:13 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,14 +1,38 @@
 <html>
  <head>
   <title>PHP Test</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="macro.js"></script>
  </head>
  <body>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="GET.php"></script>
-    <script src="macro.js"></script>
-     
- <?php echo '<p>Hello World</p>'; ?> 
-     
+
+
+<script>
+function showHint(str) {
+    if (str.length == 0) { 
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET", "gethint.php?q=" + str, true);
+        xmlhttp.send();
+    }
+}
+</script>
+</head>
+<body>
+
+<p><b>Start typing a name in the input field below:</b></p>
+<form> 
+First name: <input type="text" onkeyup="showHint(this.value)">
+</form>
+<p>Suggestions: <span id="txtHint"></span></p>
+
  </body>
 </html>
 
