0e2b703c3d808fe9d04ce29b8fc3d8a6edaa6230
daltongray <dalton.roger.gray@gmail.com>
2017-08-24 16:59:45 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,25 +1,17 @@
-<!doctype html>
-<html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
- 
-        <link rel="stylesheet" href="css/normalize.min.css">
-        <link rel="stylesheet" href="css/main.css">
- 
-       
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script src="GET.php"></script>
-
-        <script src="macro.js"></script>
-    </head>
-    <body>
-        <p>This is a basic HTML sandbox.</p>
-    </body>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="GET.php"></script>
+    <script src="macro.js"></script>
+     
+ <?php echo '<p>Hello World</p>'; ?> 
+     
+ </body>
 </html>
+
 
 <?php
 // do php stuff
