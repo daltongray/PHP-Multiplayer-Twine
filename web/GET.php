72f7d231f6489fc7af97f2eb5f8b1be78f2fae89
daltongray <dalton.roger.gray@gmail.com>
2017-08-24 16:12:34 -0400

    Update GET.php

--- web/GET.php
+++ web/GET.php
@@ -1,13 +1,14 @@
 <?php
 
-echo "Hello World!";
-
-?>
-
+echo "Hello World!" +  $_GET;
 
 // ini_set("allow_url_fopen", 1);
 
 // $json = file_get_contents($_GET);
 // $obj = json_decode($json, true);
 // echo $obj[var1];
+
+?>
+
+
 
