e04c6dd70fda911a26a8d5af0e038f0b8793bded
daltongray <dalton.roger.gray@gmail.com>
2017-08-24 17:04:19 -0400

    Update GET.php

--- web/GET.php
+++ web/GET.php
@@ -1,13 +1,58 @@
 <?php
-// ini_set("allow_url_fopen", 1);
-echo "Hello World!";
+// Array with names
+$a[] = "Anna";
+$a[] = "Brittany";
+$a[] = "Cinderella";
+$a[] = "Diana";
+$a[] = "Eva";
+$a[] = "Fiona";
+$a[] = "Gunda";
+$a[] = "Hege";
+$a[] = "Inga";
+$a[] = "Johanna";
+$a[] = "Kitty";
+$a[] = "Linda";
+$a[] = "Nina";
+$a[] = "Ophelia";
+$a[] = "Petunia";
+$a[] = "Amanda";
+$a[] = "Raquel";
+$a[] = "Cindy";
+$a[] = "Doris";
+$a[] = "Eve";
+$a[] = "Evita";
+$a[] = "Sunniva";
+$a[] = "Tove";
+$a[] = "Unni";
+$a[] = "Violet";
+$a[] = "Liza";
+$a[] = "Elizabeth";
+$a[] = "Ellen";
+$a[] = "Wenche";
+$a[] = "Vicky";
 
-// ini_set("allow_url_fopen", 1);
+// get the q parameter from URL
+$q = $_REQUEST["q"];
 
-// $json = file_get_contents($_GET);
-// $obj = json_decode($json, true);
-// echo $obj[var1];
+$hint = "";
 
+// lookup all hints from array if $q is different from "" 
+if ($q !== "") {
+    $q = strtolower($q);
+    $len=strlen($q);
+    foreach($a as $name) {
+        if (stristr($q, substr($name, 0, $len))) {
+            if ($hint === "") {
+                $hint = $name;
+            } else {
+                $hint .= ", $name";
+            }
+        }
+    }
+}
+
+// Output "no suggestion" if no hint was found or output correct values 
+echo $hint === "" ? "no suggestion" : $hint;
 ?>
 
 
