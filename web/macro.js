b5c5ce50ab58cd016f568d25d60b02f64c49fd92
daltongray <dalton.roger.gray@gmail.com>
2017-08-24 12:46:44 -0400

    Create macro.js

---
+++ web/macro.js
@@ -0,0 +1,25 @@
+
+var dataJSON = {"key":"val"}
+
+jQuery.ajax({
+			url: 'index.php',
+			method: method,
+			data: dataJSON,
+			dataType: 'json',
+			success: function (data, status, jqXHR) {
+				console.log("Success! Here's the data is returned: ", data);
+				console.log("Text status: ", status);
+				console.log("HTTP Request response: ", jqXHR);
+
+				if (todo == "get_data") {
+					return data;
+				} else if (todo == "update_data") {
+					return "Data updated, response from server: " + data;
+				} 
+
+			},
+			error: function (jqXHR, status) {
+				console.log("AJAX Error: ", jqXHR);
+				console.log("Error tText status: ", status);
+			}
+		})
