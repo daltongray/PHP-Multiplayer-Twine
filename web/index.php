92a9b6a43398ba01b6d85daf76b6f80bffb1fec1
daltongray <dalton.roger.gray@gmail.com>
2017-09-05 21:26:30 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,46 +1,34 @@
+<!DOCTYPE html>
 <html>
- <head>
-  <title>PHP Test</title>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="macro.js"></script>
- </head>
- <body>
+<body>
 
+<h1>The XMLHttpRequest Object</h1>
+
+<h3>Start typing a name in the input field below:</h3>
+
+<form action=""> 
+First name: <input type="text" id="txt1" onkeyup="showHint(this.value)">
+</form>
+
+<p>Suggestions: <span id="txtHint"></span></p> 
 
 <script>
 function showHint(str) {
-    if (str.length == 0) { 
-        document.getElementById("txtHint").innerHTML = "nothing is in the text box.";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET", "GET.php?q=" + str, true);
-        xmlhttp.send();
+  var xhttp;
+  if (str.length == 0) { 
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  }
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("txtHint").innerHTML = this.responseText;
     }
+  };
+  xhttp.open("GET", "gethint.php?q="+str, true);
+  xhttp.send();   
 }
 </script>
-</head>
-<body>
 
-<p><b>Start typing a name in the input field below:</b></p>
-<form> 
-First name: <input type="text" onkeyup="showHint(this.value)">
-</form>
-<p>Suggestions: <span id="txtHint"></span></p>
-
- </body>
+</body>
 </html>
-
-
-<?php
-// do php stuff
-
-//include('http://code.jquery.com/jquery-latest.min.js');
-//include('macro.js');
-
-?>
