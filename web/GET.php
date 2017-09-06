af16af3c29594b6d202fdb55f172813388970655
daltongray <dalton.roger.gray@gmail.com>
2017-09-05 22:49:31 -0400

    Update GET.php

--- web/GET.php
+++ web/GET.php
@@ -14,14 +14,14 @@ if ($q !== "") {
         $hint = "this JSON was mal formed";
         retun;
     // otherwise, we're assuming q was a good json
-    };/* else { 
+    } else { 
         //open up our JSON file
         $vardb = fopen("vardb.txt", "r");
         // decode it into something PHP can read
         $vardbjson = json_decode($vardb, true);
         //put it in hint so we can echo it back to the client later
         $hint = $vardbjson['$decodedjson['var']'];
-    };*/
+    };
 };
 // Output, which is either an error message or the variable we were looking for
 echo $hint;
