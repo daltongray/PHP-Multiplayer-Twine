512189c45d13bb8cf6315cea03742887fcbf3201
daltongray <dalton.roger.gray@gmail.com>
2017-09-05 21:05:12 -0400

    Update GET.php

--- web/GET.php
+++ web/GET.php
@@ -7,12 +7,16 @@ $snakefound = false;
 
 // get the q parameter from URL
 $myJSON = $_REQUEST[q];
+// Decode the JSON we received from Q
 $myObj = json_decode($myJSON, true);
-if ($myJSON !== "") {
+// if it's not a JSON it will retun NULL
+if ($myObj !== "NULL") {
+    //returns the var value of the JSON it was fed.
     echo $myObj['var'];
-    } elseif ($myJSON == NULL) {
-    echo "this is not a JSON string";
-};
+    //otherwise...
+    } else {
+    // tell us it's not a string and print the stuff we got
+    echo "this is not a JSON string, the value i got was" + $myJSON
 
 /*
 $hint = "";
