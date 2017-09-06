30401860bb9426bf5a4c8846351acd1869d4274b
daltongray <dalton.roger.gray@gmail.com>
2017-09-05 21:26:47 -0400

    Update GET.php

--- web/GET.php
+++ web/GET.php
@@ -1,18 +1,39 @@
 <?php
-// get the q parameter from URL
-$myJSON = $_REQUEST[q];
-// Decode the JSON we received from Q
-$myObj = json_decode($myJSON, true);
-// if it's not a JSON it will retun NULL
-if ($myObj !== "NULL") {
-    //returns the var value of the JSON it was fed.
-    echo $myObj['var'];
-    //otherwise...
-    } else {
-    // tell us it's not a string and print the stuff we got
-    echo "this is not a JSON string, the value i got was" + $myJSON};
+// Array with names
+$a[] = "Anna";
+$a[] = "Brittany";
+$a[] = "Cinderella";
+$a[] = "Diana";
+$a[] = "Eva";
+$a[] = "Fiona";
+$a[] = "Gunda";
+$a[] = "Hege";
+$a[] = "Inga";
+$a[] = "Johanna";
+$a[] = "Kitty";
+$a[] = "Linda";
+$a[] = "Nina";
+$a[] = "Ophelia";
+$a[] = "Petunia";
+$a[] = "Amanda";
+$a[] = "Raquel";
+$a[] = "Cindy";
+$a[] = "Doris";
+$a[] = "Eve";
+$a[] = "Evita";
+$a[] = "Sunniva";
+$a[] = "Tove";
+$a[] = "Unni";
+$a[] = "Violet";
+$a[] = "Liza";
+$a[] = "Elizabeth";
+$a[] = "Ellen";
+$a[] = "Wenche";
+$a[] = "Vicky";
 
-/*
+// get the q parameter from URL
+$q = $_REQUEST["q"];
+
 $hint = "";
 
 // lookup all hints from array if $q is different from "" 
@@ -29,10 +50,7 @@ if ($q !== "") {
         }
     }
 }
-*/
+
 // Output "no suggestion" if no hint was found or output correct values 
-// echo $hint === "" ? "no suggestion" : $hint;
+echo $hint === "" ? "no suggestion" : $hint;
 ?>
-
-
-
