0bbb1b8ac9bfa81f939e3aff302922889d8263f9
daltongray <dalton.roger.gray@gmail.com>
2017-09-30 19:23:03 -0400

    Update NewPlayer.php

--- web/NewPlayer.php
+++ web/NewPlayer.php
@@ -1,3 +1,16 @@
-<php>
+<?php
 
-</php>
+
+//pull request out of url, get the q parameter from URL
+$q = $_REQUEST["q"];
+
+	//[variable] = dejsonify
+     $decodedjson = json_decode($q, true);
+    //if it's a poorly formed json it will come back as an error
+    if ($decodedjson == NULL) {
+        //if so, set hint to an err message so that prints to the client
+        $response = "this JSON was mal formed";
+        //otherwise...
+    } 
+
+?>
