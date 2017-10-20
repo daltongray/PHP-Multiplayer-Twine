6bd9b195870118575c2b4cf23573377aa510eb7a
daltongray <dalton.roger.gray@gmail.com>
2017-10-20 17:16:49 -0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -197,7 +197,7 @@ var GET = function() {
  
   xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState == 4 && this.status == 200) {   console.log(this.responseText);
       var ReceivedResponse = this.responseText;         console.log(ReceivedResponse);
       var RRObject = JSON.parse(ReceivedResponse);
       console.log(RRObject.ErrorMessage);
