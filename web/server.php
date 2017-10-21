c535cf81f10d3edb779ebdc0dd6b0e886d31e4f0
daltongray <dalton.roger.gray@gmail.com>
2017-10-21 11:29:43 -0400

    Update server.php

--- web/server.php
+++ web/server.php
@@ -18,49 +18,55 @@ $Response = array("ErrorMessage"=>"The Response Object has been setup. ");
 
 //----------------------------------------------------------------------------------------------------------------------------
 // ----------------ID JSON, UNPACK, SETUP-----------------------------------------------------------------------
-		    $q = $_REQUEST["q"];
-	$IncomingArray = json_decode($q, true);
+		  $q = $_REQUEST["q"];
+      $IncomingArray = json_decode($q, true);
+                $Url = "TestJSON.txt";
+       $TestContents = file_get_contents("$Url");	           	  $Response['ErrorMessage'] .= "[TestContents] is ${TestContents}. ";
+   $TestContentsJSON = json_decode($TestContents, true); 		 
+         $PlayerFile = $TestContentsJSON;
+           $Protocol = $IncomingArray['Protocol'];			  $Response['ErrorMessage'] .= "[Method] is ${Protocol}. ";
+	   unset($IncomingArray['Protocol']);
 
 
 if ($IncomingArray == NULL) {
        	$Response['ErrorMessage'] .= "The JSON sent to the server was mal formed. ";
 	echo json_encode($Response);
 	return;
-}; 								      
-
-$Response['ErrorMessage'] .= "The JSON sent to the server was well formed. ";
+}; 								      	  $Response['ErrorMessage'] .= "The JSON sent to the server was well formed. ";
 
 
-
-
-
-
-
-
-$Protocol = $IncomingArray['Protocol'];			  $Response['ErrorMessage'] .= "[Method] is ${Protocol}. ";
-
+//-----------------------------------------------------------------
+// -------------------  GET   -------------------------------------
 
 if ($Protocol == "TESTGET") {
-	
-unset($IncomingArray['Protocol']);
-	
-                $Url = "TestJSON.txt";
-       $TestContents = file_get_contents("$Url");	           	  $Response['ErrorMessage'] .= "[TestContents] is ${TestContents}. ";
-   $TestContentsJSON = json_decode($TestContents, true); 		 
-//$TestContentsVarDump = var_dump($TestContentsJSON);           	 	  $Response['ErrorMessage'] .= "The Var Dump of Test Contents JSON is ${TestContentsVarDump}"; 
-        
-
-$PlayerFile = $TestContentsJSON;
-
-$ArrayIncoming = array_flip($IncomingArray);
-unset($ArrayIncoming['']);
-
-$PlayerFile = array_intersect_key($PlayerFile, $ArrayIncoming);
-$PlayerFile = $PlayerFile + $Response;
-echo json_encode($PlayerFile);
-return;
+	$ArrayIncoming = array_flip($IncomingArray);
+	unset($ArrayIncoming['']);
+	$PlayerFile = array_intersect_key($PlayerFile, $ArrayIncoming);
+	$PlayerFile = $PlayerFile + $Response;
+	echo json_encode($PlayerFile);
+	return;
 };
 
+//-----------------------------------------------------------------
+// ----------------  UPDATE   -------------------------------------
 
+
+if($Protocol = "Update") {
+    $vardb = '{"FirstName":"Quasar","LastName":"Gray"}'; //file_get_contents("$filename") or die("could not reach $filename");
+    $vardbjson = json_decode($vardb, true);
+
+    foreach ($IncomingArray as $key => $value) {
+    $PlayerFile[$key] = $value;
+    };						$Response['ErrorMessage'] .= json_encode($vardbjson);
+
+    if (is_writable($Url)) {
+        $fh = fopen("$Url", 'w') or die("Error opening output file");
+        fwrite($fh, json_encode($PlayerFile,JSON_UNESCAPED_UNICODE));
+        fclose($fh);
+    };
+
+    $Response['UpdateReport'] = "Successfully updated the PlayerFile.";
+    echo json_encode($Response);	
+};
 
 ?>
